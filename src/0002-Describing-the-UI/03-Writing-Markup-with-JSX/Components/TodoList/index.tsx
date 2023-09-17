e8067d57d90cd39920906d348036618bb9c96b03
GuiDev45 <bortolettodev@gmail.com>
2023-09-17 18:46:13 -0300

    0002 - 03 - Writing Markup with JSX

---
+++ src/0002-Describing-the-UI/03-Writing-Markup-with-JSX/Components/TodoList/index.tsx
@@ -0,0 +1,28 @@
+export default function TodoList() {
+  return (
+    <>
+      <h1>Spider Man!</h1>
+      <img src="spier-man-01.JPG" alt="Picture Spider man" className="photo" />
+      <ul>
+        <li>Superpoderes Aracnídeos</li>
+        <li>Identidade Dupla</li>
+        <li>Grande Responsabilidade</li>
+      </ul>
+    </>
+  );
+}
+
+/*
+1. Retorne um único elemento raiz
+Para retornar vários elementos de um componente, envolva-os com uma única tag pai.
+
+2. Feche todas as tags
+JSX exige que as tags sejam explicitamente fechadas: tags de fechamento automático como <img>devem se tornar <img />e tags de empacotamento como <li>orangesdevem ser escritas como <li>oranges</li>.
+
+Os componentes do React agrupam a lógica de renderização junto com a marcação porque estão relacionados.
+JSX é semelhante ao HTML, com algumas diferenças. Você pode usar um conversor se precisar.
+As mensagens de erro geralmente apontam a direção certa para corrigir sua marcação.
+
+Resumo
+JSX é uma sintaxe, uma extensão da linguagem JavaScript que permite incorporar código HTML em JavaScript de forma mais conveniente, tornando a criação de interfaces de usuário em frameworks como o React mais simples e legível.
+*/
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import "./Style.css";
 // Escolher qual caminho de exercício deseja importar.
 // import App from "./0001-Exercicios/App";
 // import App from "./0002-Describing-the-UI/01-Your-First-Component/App";
-import App from "./0002-Describing-the-UI/02-Importing-and-Exporting-Components/App";
+// import App from "./0002-Describing-the-UI/02-Importing-and-Exporting-Components/App";
+import App from "./0002-Describing-the-UI/03-Writing-Markup-with-JSX/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
